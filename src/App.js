31cfeab5a4c689bada73a7b6fefef561ecd7e062
Grishenkohelen <88398523+Grishenkohelen@users.noreply.github.com>
2022-09-24 21:50:55 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import {Posts} from "./components";
+import {Users} from "./components/users/Users";
+import {useState} from "react";
+
+import {postService} from "./services";
+
 function App() {
+   const [posts, setPosts] = useState([]);
+
+   const getUseId =(userId)=>{
+        postService.getPosts(userId).then(({data})=> setPosts(data) )
+   }
   return (
     <div>
-
+      <Users  getUseId = {getUseId}/>
+      <Posts posts={posts}/>
     </div>
   );
 }
