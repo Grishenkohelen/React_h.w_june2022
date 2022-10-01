d35efe5c5fc281b8624a59494273a309c88d24b2
Grishenkohelen <88398523+Grishenkohelen@users.noreply.github.com>
2022-10-01 23:54:03 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import {Route, Routes} from "react-router-dom";
+
+import {MainLayout} from "./layout/MainLayout";
+import {AlbumsPage} from "./pages/AlbumsPage";
+import {CommentsPage} from "./pages/CommentsPage";
+import {TodosPage} from "./pages/TodosPage";
+import {Posts} from "./components";
+
+
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
-}
+    return(<div>
+        <Routes>
+            <Route path={'/'} element ={<MainLayout/>}>
+                <Route path={'albums'} element={<AlbumsPage/>}/>
+                <Route path={'comments'} element ={<CommentsPage/>}>
+                       <Route path={':postId'} element={<Posts/>}/>
+            </Route>
 
+            <Route path={'todos'} element={<TodosPage/>}/>
+            </Route>
+        </Routes>
+    </div>);
+
+      }
 export default App;
+
+
+
+
