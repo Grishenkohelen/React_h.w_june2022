516aa47594a421580a3e4cc3c4e788e4c4423183
Grishenkohelen <88398523+Grishenkohelen@users.noreply.github.com>
2022-10-02 21:26:56 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div>
+import {useReducer} from "react";
 
-    </div>
-  );
+const init = (initialValue) => {
+  return {counter1: initialValue, counter2:initialValue}
 }
 
-export default App;
+const reducer = (state, action)=>{
+  return {...state}
+}
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, 0, init);
+  return (
+    <div>
+      <h1>counter1:{state.counter1}</h1>
+      <h1>counter2:{state.counter2}</h1>
+        <button>inc</button>
+        <button>dec</button>
+        <button>reset</button>
+    </div>
+  );
+};
+
+export  {App};
